194dae8921970283a85e5b4c452f4c42b966f903
PEDRO ABRAHAO BARROS <pedro.barros3@GRANBERY.local>
2024-09-03 21:53:00 -0300

    Adicionando head + navbar

--- FlamengoStore/src/app/app.module.ts
+++ FlamengoStore/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './principal/principal.component';
+import { HeadComponent } from './head/head.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrincipalComponent
+    PrincipalComponent,
+    HeadComponent
   ],
   imports: [
     BrowserModule,
